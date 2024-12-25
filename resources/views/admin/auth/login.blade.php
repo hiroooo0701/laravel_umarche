1cbf1974bae1ccb7e0ff5004ad46741f115174f7
hiro <hiroaki.fukuda@manpowergroup.jp>
2024-12-25 16:02:25 +0900

    carbon

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,9 +2,9 @@
     <x-auth-card>
         管理者用
         <x-slot name="logo">
-            <div class="w-5">
+            <div class="w-8">
             <a href="/">
-                <x-application-logo class="w-5 h-5 fill-current text-red-300" />
+                <x-application-logo class="w-20 h-20 fill-current text-red-500" />
             </a>
             </div>
         </x-slot>
